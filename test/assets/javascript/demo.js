cc0163d97a947b280e94ae3ea6fe2959d86f091e
Uditha Atukorala <fake@thewireframe>
2012-09-19 12:31:48 +0100

    updating demo / test

--- test/assets/javascript/demo.js
+++ test/assets/javascript/demo.js
@@ -4,7 +4,10 @@ window.addEvent('domready', function(){
 	var pagination = new xPagination({
 		element_left   : $('pag-content-01'),
 		element_active : $('pag-content-02'),
-		element_right  : $('pag-content-03')
+		element_right  : $('pag-content-03'),
+		onPagechange: function( element, page ) {
+			console.log('[options]: load "' + element.id +  '" with page ' + page );
+		}
 	});
 
 	$$('.previous').addEvent('click', function( e ) {
@@ -15,6 +18,10 @@ window.addEvent('domready', function(){
 	$$('.next').addEvent('click', function( e ) {
 		e.stop();
 		pagination.next();
+	});
+
+	pagination.addEvent( 'pagechange', function( element, page ){
+		console.log('[event handler]: load "' + element.id +  '" with page ' + page );
 	});
 
 });
