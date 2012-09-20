d58d49bf87732a7ecc80b5c061752ed487317cac
Uditha Atukorala <fake@thewireframe>
2012-09-20 15:26:05 +0100

    introducing xPagination.JSON class and working towards version 2.0   - with this commit the class signature for xPagination has changed     and it is no longer compatible with version 1.0

--- test/assets/javascript/demo.js
+++ test/assets/javascript/demo.js
@@ -1,14 +1,31 @@
 
+var data = {
+	total: 75,
+	template: 'template string',
+	data: [
+		{name: "John Smith", age: 26},
+		{name: "Johny Smith", age: 23},
+		{name: "John Smithy", age: 24},
+		{name: "Johny Smithy", age: 25},
+		{name: "Jo Smith", age: 27}
+	]
+};
+
+var pagination = {};
+
 window.addEvent('domready', function(){
 
-	var pagination = new xPagination({
-		element_left   : $('pag-content-01'),
-		element_active : $('pag-content-02'),
-		element_right  : $('pag-content-03'),
-		onPagechange: function( element, page ) {
-			console.log('[options]: load "' + element.id +  '" with page ' + page );
+	pagination = new xPagination.JSON(
+		$('pag-content-01'),
+		$('pag-content-02'),
+		$('pag-content-03'),
+		data,
+		{
+			onPagechange: function( element, page ) {
+				console.log('[options]: load "' + element.id +  '" with page ' + page );
+			}
 		}
-	});
+	);
 
 	$$('.previous').addEvent('click', function( e ) {
 		e.stop();
